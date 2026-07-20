bb52624f1c23af1e6768ae383a88371a1ba53140
to-do list <bdhw48796@gmail.com>
2026-07-21 01:57:41 +0300

    day 12

---
+++ script/profile.js
@@ -0,0 +1,43 @@
+var myProfile={
+    userName:`abdo waleed`,
+    age:19,
+    study:function() {
+        console.log(`computer since`);
+        console.log(`uni of O6U /n up to level 2`);
+        
+    },
+    level:2,
+    skill:function(level) {
+        if (level==1) {
+            console.log(`skills is cpp`);
+        } else if (level>1&&level<3){
+            console.log(`skills is cpp and front end`);
+            
+        }else {
+            console.error(`don't know`);    
+        }
+    }
+    
+}
+console.table(myProfile);
+console.log(myProfile.study());
+console.log(myProfile.skill(2));
+
+function calcSumNum(num){
+    var result=0;
+    for (var i=1;i<num;i++){
+        num=i;
+    }
+    result=num;
+    console.log(`result = ${result}`);
+    
+}
+calcSumNum(3); 
+(
+    function(){
+        var x=6;
+        x=9;
+        console.log(x);
+        x=8;  
+    }
+)()       
